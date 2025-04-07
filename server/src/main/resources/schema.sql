e430bd02914fa8f36d13d6abd9e6e784b283beef
ycheng11065 <ychengli11065@gmail.com>
2025-04-06 18:25:42 -0700

    created earthquake database table

--- server/src/main/resources/schema.sql
+++ server/src/main/resources/schema.sql
@@ -1,4 +1,5 @@
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
 CREATE TABLE IF NOT EXISTS satellites (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     norad_cat_id INT UNIQUE,
@@ -17,4 +18,28 @@ CREATE TABLE IF NOT EXISTS satellites (
     apoapsis DOUBLE PRECISION,
     periapsis DOUBLE PRECISION,
     semimajor_axis DOUBLE PRECISION
+);
+
+CREATE TABLE IF NOT EXISTS earthquakes (
+   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+   magnitude DOUBLE PRECISION,
+   location_description VARCHAR(255),
+   event_time TIMESTAMP,
+   usgs_update_time TIMESTAMP,
+   timezone_offset_minutes INT,
+   community_intensity_cdi DOUBLE PRECISION,
+   mercalli_intensity_mmi DOUBLE PRECISION,
+   usgs_alert_level VARCHAR(20),
+   processing_status VARCHAR(20),
+   tsunami_potential INT,
+   event_significance INT,
+   station_count INT,
+   min_station_distance_deg DOUBLE PRECISION,
+   event_type VARCHAR(50),
+   epicenter_longitude DOUBLE PRECISION,
+   epicenter_latitude DOUBLE PRECISION,
+   depth_km DOUBLE PRECISION,
+   known_event_ids TEXT,
+   preferred_event_id VARCHAR(50) UNIQUE,
+   last_updated TIMESTAMP
 );
