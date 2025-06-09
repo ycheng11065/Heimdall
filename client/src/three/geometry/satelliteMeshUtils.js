4fef2a688fb970e397567d7c5ae1c72a85dcdbb1
ycheng11065 <ychengli11065@gmail.com>
2025-06-08 20:23:04 -0700

    fixed satellite static data disseapearing

--- client/src/three/geometry/satelliteMeshUtils.js
+++ client/src/three/geometry/satelliteMeshUtils.js
@@ -3,21 +3,23 @@ import * as THREE from 'three';
 import { GLOBE, EARTH } from '../constants.js';
 
 export function createSatelliteMesh(satelliteDTO, position, latitude, longitude, altitude, color = 0xff0000) {
-	// const scale = GLOBE.RADIUS / EARTH.MEAN_RADIUS;
-	const scale = 1 / EARTH.MEAN_RADIUS;
-
-
 	const geometry = new THREE.SphereGeometry(0.1, 8, 8);
 	const material = new THREE.MeshBasicMaterial({ color });
 	const mesh = new THREE.Mesh(geometry, material);
 
-	setSatellitePosition(mesh, position, satelliteDTO, latitude, longitude, altitude);
+	mesh.userData = {
+		noradCatId: satelliteDTO.noradCatId,
+		objectName: satelliteDTO.objectName,
+		countryCode: satelliteDTO.countryCode,
+		launchDate: satelliteDTO.launchDate,
+		decayDate: satelliteDTO.decayDate,
+		lastUpdated: satelliteDTO.lastUpdated,
+		epoch: satelliteDTO.epoch,
+		tleLine1: satelliteDTO.tleLine1,
+		tleLine2: satelliteDTO.tleLine2,
+	}
 
-	// setSatellitePosition(mesh, [
-	// 	position[0] * scale,
-	// 	position[1] * scale,
-	// 	position[2] * scale
-	// ]);
+	setSatellitePosition(mesh, position, satelliteDTO, latitude, longitude, altitude);
 
 	return mesh;
 }
@@ -31,21 +33,12 @@ export function setSatellitePosition(mesh, position, satelliteDTO, latitude, lon
 		position[2] * scale
 	);
 
-	mesh.userData = {
-		noradCatId: satelliteDTO.noradCatId,
-        objectName: satelliteDTO.objectName,
-        countryCode: satelliteDTO.countryCode,
-        launchDate: satelliteDTO.launchDate,
-        decayDate: satelliteDTO.decayDate,
-        lastUpdated: satelliteDTO.lastUpdated,
-        epoch: satelliteDTO.epoch,
-        tleLine1: satelliteDTO.tleLine1,
-        tleLine2: satelliteDTO.tleLine2,
+	Object.assign(mesh.userData, {
 		x: position[0],
 		y: position[1],
 		z: position[2],
 		latitude: latitude,
 		longitude: longitude,
 		altitude: altitude 
-	}
+	});
 }
